2641a14cf61aff4cc5d472ab15546d7be14b8f7e
Exponential-Workload <cc@aaathats3as.com>
2023-05-09 21:00:19 +0200

    feat: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<div id="appRoot">
+  <slot />
+</div>
+
+<style lang="scss">
+  #appRoot {
+    // no blinding ppl
+    background: #1a1a1a;
+    color: #ffffff;
+    // remove ugly default fonts
+    font-family: Comme, Inter, 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; // Inter is imported in app.html
+    // positioning
+    position: fixed;
+    top: 0;
+    left: 0;
+    // display control
+    width: 100vw;
+    height: 100vh;
+    // overflow control
+    overflow-y: auto;
+    overflow-x: hidden;
+    // center everything
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    :global(a) {
+      color: #88aaff;
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+  import { goto } from '$app/navigation';
+  import { onMount } from 'svelte';
+
+  // get params 'id', 'perm' and 'scope' from query
+  let id: string, perm: string, scope: string;
+  let loaded = false;
+  let rediring = false;
+  const getPerm = (perm: string, ours: boolean) =>
+    `&${ours ? 'p' : 'permissions'}=${encodeURIComponent(perm)}`;
+  const getScope = (scope: string, ours: boolean) =>
+    `&${ours ? 's' : 'scope'}=${encodeURIComponent(scope)}`;
+  const getLink = (id: string, perm: string, scope: string, ours = false) =>
+    `${
+      ours
+        ? `${window.location.protocol}//${
+            document.location.host ?? document.location.hostname
+          }/?id`
+        : 'https://discord.com/api/oauth2/authorize?client_id'
+    }=${encodeURIComponent(id)}${
+      ours ? (perm === '8' ? '' : getPerm(perm, ours)) : getPerm(perm, ours)
+    }${
+      ours
+        ? scope === 'bot application.commands'
+          ? ''
+          : getScope(scope, ours)
+        : getScope(scope, ours)
+    }`;
+  onMount(() => {
+    const query = new URLSearchParams(window.location.search);
+    id = query.get('id') ?? '';
+    perm = query.get('perm') ?? query.get('p') ?? '8';
+    scope = query.get('scope') ?? query.get('s') ?? 'bot applications.commands';
+    if (id && perm && scope) {
+      goto(getLink(id, perm, scope));
+      rediring = true;
+    }
+    loaded = true;
+  });
+</script>
+
+<svelte:head>
+  <title>/</title>
+  <meta name="description" content="bot stuff" />
+</svelte:head>
+
+<main style="--yee: {id ? 'rgb(170, 255, 170)' : 'rgba(255, 170, 170, 0.3)'}">
+  {#if loaded}
+    {#if rediring}
+      <h1>Redirecting...</h1>
+    {:else}
+      <span>Client ID</span>
+      <input type="text" bind:value={id} placeholder="1105363089047695328" /><br
+      />
+      <span>Bot Permissions</span>
+      <input
+        type="number"
+        bind:value={perm}
+        placeholder="Client Permissions"
+      /><br />
+      <span>Client Scope</span>
+      <input type="text" bind:value={scope} placeholder="Client Scope" />
+      <div style="margin-bottom:8px;" />
+      {#if id}
+        <a href={getLink(id, perm, scope, true)}
+          >{getLink(id, perm, scope, true)}</a
+        >
+      {:else}
+        <p style="display: inline-block;margin: 0 0;color: #faa">
+          No Client ID
+        </p>
+      {/if}
+    {/if}
+  {:else}
+    <h1>Loading...</h1>
+  {/if}
+</main>
+
+<style lang="scss">
+  main {
+    filter: drop-shadow(0px 0px 4px var(--yeee));
+    span,
+    input {
+      width: 202px;
+      display: inline-block;
+    }
+    span {
+      text-align: right;
+    }
+    input {
+      color: #fff;
+      background: #333;
+      border: 1px solid #555;
+    }
+  }
+</style>
